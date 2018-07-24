6bd9e2b8d331b0b39c3a2ea6e70f602be58cc603
carvalhova <carvalhova@hotmail.com>
2018-07-24 16:55:43 -0300

    Initial version

---
+++ AspNetCoreGlobalErrorHandling/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using AspNetCoreGlobalErrorHandling.Extensions;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+
+namespace AspNetCoreGlobalErrorHandling
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerManager logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.ConfigureExceptionHandler(logger);
+            //app.ConfigureCustomExceptionMiddleware();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
